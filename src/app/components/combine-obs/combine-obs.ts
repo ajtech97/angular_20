bae2206c420b84ab9c2904a1755696d541fb342b
ajay <ajay.l@intellectselect.com>
2026-01-05 10:04:23 +0530

    switchmap, mergemap, concatmap, exhaustmap

--- src/app/components/combine-obs/combine-obs.ts
+++ src/app/components/combine-obs/combine-obs.ts
@@ -1,10 +1,11 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, inject } from '@angular/core';
-import { forkJoin, of } from 'rxjs';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { concatMap, exhaustMap, forkJoin, mergeMap, of, Subject, switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-combine-obs',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './combine-obs.html',
   styleUrl: './combine-obs.css',
 })
@@ -13,23 +14,69 @@ export class CombineObs {
   stateData$ = of(["MP", "MH", "GOA"])
   cityData$ = of(["Pune", "Nagpur", "Mumbai"])
 
+  search = new FormControl()
+
+  login$ = new Subject<void>()
+
   http = inject(HttpClient)
 
   constructor() {
 
+    // ForkJoin - combine multiple subscription : eg- dashboard
+
     forkJoin([this.stateData$, this.cityData$]).subscribe((res) => {
-      debugger
+      // debugger
     })
 
     const $users = this.http.get("https://jsonplaceholder.typicode.com/users")
     const $posts = this.http.get("https://jsonplaceholder.typicode.com/posts")
 
     forkJoin([$users, $posts]).subscribe((res) => {
-      debugger
+      // debugger
     }, error => {
-      debugger
+      // debugger
     })
 
+
+    // switchMap - Latest api will be triggered and previous api should get cancle out
+
+    // this.search.valueChanges.pipe(
+    //   switchMap((search: string) => this.http.get("https://dummyjson.com/products/search?q=" + search))
+    // ).subscribe((data) => {
+    //   console.log(data)
+    // })
+
+
+    // mergeMap - All api will be triggered 
+
+    // this.search.valueChanges.pipe(
+    //   mergeMap((search: string) => this.http.get("https://dummyjson.com/products/search?q=" + search))
+    // ).subscribe((data) => {
+    //   console.log(data)
+    // })
+
+    // concatMap - One api will get executed at a time 
+
+    // this.search.valueChanges.pipe(
+    //   concatMap((search: string) => this.http.get("https://dummyjson.com/products/search?q=" + search))
+    // ).subscribe((data) => {
+    //   console.log(data)
+    // })
+
+    // exhaustMap - It wont trigger another api call if one api is in active state
+
+    this.login$.pipe(
+      exhaustMap(() => {
+        return this.http.get("https://jsonplaceholder.typicode.com/users")
+      })
+    ).subscribe((data) => {
+
+    })
+
+  }
+
+  onLogin() {
+    this.login$.next()
   }
 
 }
